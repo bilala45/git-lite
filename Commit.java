a5fd208b9f2f35220bbaf76be048f8dad74f27d1
Bilal Ali <bilalal495@outlook.com>
2022-07-07 22:55:46 -0400

    create commit class and add instance variables

---
+++ Commit.java
@@ -0,0 +1,13 @@
+package gitlite;
+
+public class Commit {
+
+  // instance variables
+  // commit message
+  private String commitMessage;
+  // date of commit
+  private Date commitDate;
+  // parent commit
+  private Commit parent;
+
+}
